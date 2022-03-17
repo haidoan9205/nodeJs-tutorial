ca22d10ae47e2b12e6396e223e22fe4920e4d7f3
haidoan9205 <haidoan9205@gmail.com>
2022-03-17 13:55:21 +0700

    start with nodejs

---
+++ 02-modules.js
@@ -0,0 +1,11 @@
+//modules
+const names = require('./03-name')
+const sayHi = require('./05-utils')
+const data = require('./06-alternative-flavor')
+// console.log(data);
+
+require('./07-mind-grenade')
+
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.peter)
